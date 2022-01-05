e8541649cf88b354ce1e32b8bd9758a7d8690730
Susana <susanamncwabe@outlook.com>
2022-01-05 14:50:43 +0000

    Touch_out done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -36,7 +36,11 @@ require 'oystercard'
     subject.touch_in
     expect(subject).to be_in_journey
    end 
-
+   it 'response to touch_out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+   end 
 
   end
 
